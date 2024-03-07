a6f04263fa32cbd9aaddb404b3dd6866db00f15a
Thinh Nguyen <ngtrphuocthinh@gmail.com>
2024-03-07 20:29:24 +0000

    update game

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-import re
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QPushButton,QGraphicsOpacityEffect
-from PyQt5.QtGui import QPixmap,QPainter
+from PyQt5.QtWidgets import QApplication,  QWidget, QGridLayout, QLabel
+from PyQt5.QtGui import QBrush,  QPixmap, QPainter
 
 from game import Game
 from position import Position
@@ -13,21 +12,17 @@ class CardGrid(QWidget):
         super().__init__(parent)
         if game is None:
             game = Game()
-        self.setWindowTitle("Card Grid")
-        self.setGeometry(80, 80, 1000, 1000)
+        self.setWindowTitle("Romeo and Juliet")
+        self.grid_layout = QGridLayout()
+        self.grid_layout.setSpacing(3)
+        self.setLayout(self.grid_layout)
+
         self.game = game
         self.is_move = False
         self.is_swap = False
         self.possible_clicks = []
-        self.grid_layout = QGridLayout()
-        self.setLayout(self.grid_layout)
-        self.affected_widgets = []
-
+        self.picked_joker = None
         self.create_grid()
-        self.red_romeo()
-        self.black_romeo()
-        self.resize(600, 600)
-        self.button = QPushButton("Click me")  # Button creation moved here
 
     def card_clicked(self, position_x, position_y, event):
         print(f"position clicked {position_x} {position_y}")
@@ -37,12 +32,12 @@ class CardGrid(QWidget):
             self.is_swap = False
             self.possible_clicks = []
             self.create_grid()
-            self.red_romeo()
-            self.black_romeo()
-            self.remove_effects_and_styles()
+            self.place_red_romeo()
+            self.place_black_romeo()
             return
         if self.is_swap and Position(position_x, position_y) in self.possible_clicks:
-            self.game.perform_swap(Position(position_x, position_y))
+            self.game.perform_swap(
+                self.picked_joker, Position(position_x, position_y))
             self.is_move = False
             self.is_swap = False
             self.possible_clicks = []
@@ -54,13 +49,16 @@ class CardGrid(QWidget):
             self.highlight_cards(possible_moves)
             self.possible_clicks = possible_moves
             self.is_move = True
+            self.is_swap = False
             return
         position = self.is_joker_position(position_x, position_y)
         if position is not None:
+            self.picked_joker = position
             possible_swaps = self.game.list_possible_swaps(position)
             self.highlight_cards(possible_swaps)
             self.possible_clicks = possible_swaps
             self.is_swap = True
+            self.is_move = False
             return
 
         self.show_message()
@@ -77,32 +75,33 @@ class CardGrid(QWidget):
 
     def highlight_cards(self, positions):
         game = self.game
-        board = game.board  
-        for row in range(game.MATRIX_SIZE):
-            for col in range(game.MATRIX_SIZE):
-                found = False
-                for position in positions:
-                    if position.x == row and position.y == col:
-                        found = True
-                        cell_widget = self.grid_layout.itemAtPosition(row, col).widget()
-                        cell_widget.setStyleSheet("border: 3px solid red")
-                        break
+        for position in self.possible_clicks:
+            card = self.grid_layout.itemAtPosition(
+                position.x, position.y).widget()
+            original_pixmap = QPixmap(
+                game.board[position.x][position.y].get_image_path())
+            original_pixmap = original_pixmap.scaledToWidth(
+                64, Qt.TransformationMode.SmoothTransformation)
+            card.setPixmap(original_pixmap)
 
-                if not found:
-                    cell_widget = self.grid_layout.itemAtPosition(row, col).widget()
-                    opacity_effect = QGraphicsOpacityEffect()
-                    opacity_effect.setOpacity(0.5)  # Adjust opacity as needed
-                    cell_widget.setGraphicsEffect(opacity_effect)
-                    cell_widget.setStyleSheet("border: 2px solid yellow")
-                    self.affected_widgets.append(cell_widget)
-       
-          
-            # QTimer.singleShot(1000, lambda: cell_widget.setGraphicsEffect(None))
+        for position in positions:
+            card = self.grid_layout.itemAtPosition(
+                position.x, position.y).widget()
+            original_pixmap = QPixmap(
+                game.board[position.x][position.y].get_image_path())
+            original_pixmap = original_pixmap.scaledToWidth(
+                64, Qt.TransformationMode.SmoothTransformation)
+            result_pixmap = QPixmap(original_pixmap.size())
+            result_pixmap.fill(Qt.transparent)
 
-    def remove_effects_and_styles(self):
-        for widget in self.affected_widgets:
-            widget.setGraphicsEffect(None)
-            widget.setStyleSheet("")
+            painter = QPainter(result_pixmap)
+            painter.drawPixmap(0, 0, original_pixmap)
+            painter.setOpacity(0.2)  # Set opacity to 50%
+            painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+            # Draw a rectangle covering the entire pixmap
+            painter.drawRect(result_pixmap.rect())
+            painter.end()
+            card.setPixmap(result_pixmap)
 
     def is_joker_position(self, position_x, position_y):
         joker_positions = self.game.get_swappable_jokers()
@@ -120,116 +119,57 @@ class CardGrid(QWidget):
                 item = self.grid_layout.itemAtPosition(row, col)
 
                 if item is None:
-                    print(1)
                     card_label = QLabel(str(card))
+                    card_label.setStyleSheet("border: 1px solid black")
                     self.grid_layout.addWidget(card_label, row, col)
                 else:
-                    print(2)
                     card_label = item.widget()
                     self.grid_layout.addWidget(card_label, row, col)
-
 
                 pixmap = QPixmap(card.get_image_path())
-                pixmap = pixmap.scaledToWidth(64, Qt.TransformationMode.SmoothTransformation)
+                pixmap = pixmap.scaledToWidth(
+                    64, Qt.TransformationMode.SmoothTransformation)
                 card_label.setPixmap(pixmap)  # Set or update pixmap
-                card_label.setScaledContents(True)
-                
-                card_label.mousePressEvent = lambda event, row=row, col=col: self.card_clicked(row, col, event)
+                # card_label.setScaledContents(True)
 
-    def red_romeo(self):  
+                card_label.mousePressEvent = lambda event, row=row, col=col: self.card_clicked(
+                    row, col, event)
+
+        self.place_red_romeo()
+        self.place_black_romeo()
+
+    def place_red_romeo(self):
         game = self.game
-        board = game.board
         red_position = game.red_player.position
-        row = red_position.x
-        col = red_position.y
 
-        container_label = QLabel()
-        card = board[row][col]
-        item = self.grid_layout.itemAtPosition(row, col)
+        item = self.grid_layout.itemAtPosition(red_position.x, red_position.y)
+        card_label = item.widget()
 
-        if item is None:
-                    print(1)
-                    card_label = QLabel(str(card))
-                    self.grid_layout.addWidget(card_label, row, col)
-        else:
-                    print(2)
-                    card_label = item.widget()
-                    self.grid_layout.addWidget(card_label, row, col)
+        red_coin = QPixmap("assets/coins/red_coin.svg")
+        red_coin = red_coin.scaledToWidth(
+            54, Qt.TransformationMode.SmoothTransformation)
 
-
-        card_pixmap = QPixmap(card.get_image_path())
-        card_pixmap = card_pixmap.scaledToWidth(64, Qt.TransformationMode.SmoothTransformation)
-        container_label.setPixmap(card_pixmap)
-    
-        romeo_pixmap =QPixmap("assets/coins/red_coin.png")
-        romeo_pixmap = romeo_pixmap.scaledToWidth(64, Qt.TransformationMode.SmoothTransformation)
-
-        # Create a painter for drawing over the card image
-        painter = QPainter(container_label.pixmap())
+        painter = QPainter(card_label.pixmap())
         painter.setCompositionMode(QPainter.CompositionMode_SourceOver)
+        painter.drawPixmap(5, 20, red_coin)
+        painter.end()
 
-        # Draw the Romeo image on top of the card image
-        painter.drawPixmap(0, 0, romeo_pixmap)
-
-        painter.end()  # Set maximum size for the label to control its display size
-
-        container_label.mousePressEvent = lambda event: self.card_clicked(row, col, event)
-
-        # Remove any existing widget at this position
-        existing_item = self.grid_layout.itemAtPosition(row, col)
-        if existing_item:
-            existing_widget = existing_item.widget()
-            self.grid_layout.removeWidget(existing_widget)
-            existing_widget.deleteLater()  # Ensure proper deletion
-
-        self.grid_layout.addWidget(container_label, row, col)
-    def black_romeo(self):
+    def place_black_romeo(self):
         game = self.game
-        board = game.board  
         black_position = game.black_player.position
-        row = black_position.x
-        col = black_position.y
 
-        container_label = QLabel()
-        card = board[row][col]
-        item = self.grid_layout.itemAtPosition(row, col)
+        item = self.grid_layout.itemAtPosition(
+            black_position.x, black_position.y)
+        card_label = item.widget()
 
-        if item is None:
-                    print(1)
-                    card_label = QLabel(str(card))
-                    self.grid_layout.addWidget(card_label, row, col)
-        else:
-                    print(2)
-                    card_label = item.widget()
-                    self.grid_layout.addWidget(card_label, row, col)
+        coin = QPixmap("assets/coins/black_coin.svg")
+        coin = coin.scaledToWidth(
+            54, Qt.TransformationMode.SmoothTransformation)
 
-
-        card_pixmap = QPixmap(card.get_image_path())
-        card_pixmap = card_pixmap.scaledToWidth(64, Qt.TransformationMode.SmoothTransformation)
-        container_label.setPixmap(card_pixmap)
-    
-        romeo_pixmap =QPixmap("assets/coins/black_coin.png")
-        romeo_pixmap = romeo_pixmap.scaledToWidth(64, Qt.TransformationMode.SmoothTransformation)
-
-        # Create a painter for drawing over the card image
-        painter = QPainter(container_label.pixmap())
+        painter = QPainter(card_label.pixmap())
         painter.setCompositionMode(QPainter.CompositionMode_SourceOver)
-
-        # Draw the Romeo image on top of the card image
-        painter.drawPixmap(0, 0, romeo_pixmap)
-
-        painter.end()  # Set maximum size for the label to control its display size
-
-        container_label.mousePressEvent = lambda event: self.card_clicked(row, col, event)
-
-        # Remove any existing widget at this position
-        existing_item = self.grid_layout.itemAtPosition(row, col)
-        if existing_item:
-            existing_widget = existing_item.widget()
-            self.grid_layout.removeWidget(existing_widget)
-            existing_widget.deleteLater()  # Ensure proper deletion
-
-        self.grid_layout.addWidget(container_label, row, col)
+        painter.drawPixmap(5, 20, coin)
+        painter.end()
 
 
 if __name__ == "__main__":
